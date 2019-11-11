7e9544b7c0a6d0be1ca9646597924b40d51f9278
LenWinkler <lenwinkler86@gmail.com>
2019-11-11 12:29:18 -0500

    wrote endpoint to hash password

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -9,5 +10,21 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+  const pass = req.body.password;
+
+  const hashedPass = bcrypt.hashSync(pass, 14);
+
+  return {
+    pass: pass,
+    hashedPass: hashedPass
+  }
+
+  //read a pass from the body
+  //hash the pass
+  // return it to the user in an object that looks like
+  // { password: 'original password', hash: 'hashed password }
+})
 
 module.exports = router;
